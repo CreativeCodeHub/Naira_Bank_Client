43eec3f5e7c7e90d668627044729b2a67acf8cf2
daniel13 <102862847+DanielImgbi@users.noreply.github.com>
2024-03-23 10:07:51 -0700

    update: setting up react-query

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,7 +1,8 @@
-// module.exports = {
-//   trailingComma: "es5",
-//   tabwidth: 2,
-//   semi: true,
-//   singleQoute: false,
-//   endOfLines: "lf",
-// };
+module.exports = {
+  trailingComma: "es5",
+  tabwidth: 2,
+  semi: true,
+  singleQoute: false,
+  endOfLines: "lf",
+  jsxBracketSameLine: true,
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConfigProvider } from "antd";
 import Pages from "./pages";
+
+const queryClient = new QueryClient();
 
 export default function App(): JSX.Element {
   return (
     <ConfigProvider>
-      <Pages />
+      <QueryClientProvider client={queryClient}>
+        <Pages />
+      </QueryClientProvider>
     </ConfigProvider>
   );
 }
