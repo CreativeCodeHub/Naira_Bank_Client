1e75cb4f2da604ffd7612e1888bed0f7f58f1e54
daniel13 <102862847+DanielImgbi@users.noreply.github.com>
2024-03-24 22:50:57 -0700

    update: setting up styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConfigProvider } from "antd";
 import Pages from "./pages";
+import GlobalStyle from "./global.styles";
 
 const queryClient = new QueryClient();
 
 export default function App(): JSX.Element {
   return (
     <ConfigProvider>
-      <QueryClientProvider client={queryClient}>
-        <Pages />
-      </QueryClientProvider>
+      <GlobalStyle>
+        <QueryClientProvider client={queryClient}>
+          <Pages />
+        </QueryClientProvider>
+      </GlobalStyle>
     </ConfigProvider>
   );
 }
